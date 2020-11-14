55d662b9fb8e248a5988fd966e21389c2610277f
sjmelhuish <sjmelhuish@users.noreply.github.com>
2020-11-14 17:20:19 +0000

    Using docopt. Shrink input pic.

--- ysf-image-copy.py
+++ ysf-image-copy.py
@@ -1,11 +1,34 @@
+# -*- coding: utf-8 -*-
+""" ysf-image-copy
+
+Usage:
+  ysf-image-copy.py CALLSIGN RADIOID OUTDIR [-d DIRECTORY|-f PICFILE] [-u]
+  
+Arguments:
+  CALLSIGN            The RX location from internal list of RAS sites
+  RADIOID             The Radio ID to insert
+  OUTDIR              The output directory
+  
+Options:
+  -h --help                          Show this screen
+  -v --version                       Show version
+  -d DIRECTORY --dir=DIRECTORY       Name the input directory forbatch conversion
+  -f PICFILE --file=PICFILE         Convert a single file
+  -u                                 Update files at outdir instead of starting from scratch
+  
+"""
+
 import io
 import binascii
 from datetime import datetime, timedelta
 import os
 from PIL import Image, ExifTags
 from PIL.ExifTags import TAGS, GPSTAGS
+import pyguetzli
 
 from shutil import copyfile # Used to copy jpg. Not if we write it?
+
+from docopt import docopt
 
 photofilename = r'TestImages/StileNoExif.JPG'
 exiffilename = r'TestImages/StileExif.JPG'
@@ -33,39 +56,44 @@ def get_geotagging(exif):
 
     return geotagging
 
-def get_decimal_from_dms(dms, ref):
+# def get_decimal_from_dms(dms, ref):
 
-    degrees = dms[0][0] / dms[0][1]
-    minutes = dms[1][0] / dms[1][1] / 60.0
-    seconds = dms[2][0] / dms[2][1] / 3600.0
+#     degrees = dms[0][0] / dms[0][1]
+#     minutes = dms[1][0] / dms[1][1] / 60.0
+#     seconds = dms[2][0] / dms[2][1] / 3600.0
 
-    if ref in ['S', 'W']:
-        degrees = -degrees
-        minutes = -minutes
-        seconds = -seconds
+#     if ref in ['S', 'W']:
+#         degrees = -degrees
+#         minutes = -minutes
+#         seconds = -seconds
 
-    return round(degrees + minutes + seconds, 5)
+#     return round(degrees + minutes + seconds, 5)
 
-def get_coordinates(geotags):
-    lat = get_decimal_from_dms(geotags['GPSLatitude'], geotags['GPSLatitudeRef'])
+# def get_coordinates(geotags):
+#     lat = get_decimal_from_dms(geotags['GPSLatitude'], geotags['GPSLatitudeRef'])
 
-    lon = get_decimal_from_dms(geotags['GPSLongitude'], geotags['GPSLongitudeRef'])
+#     lon = get_decimal_from_dms(geotags['GPSLongitude'], geotags['GPSLongitudeRef'])
 
-    return (lat,lon)
+#     return (lat,lon)
 
 def encodegps(filename):
     exif = get_exif(filename)
+    # print(f"exif from {filename}:")
+    # print(exif)
     geotags = get_geotagging(exif)
-    #print(get_coordinates(geotags))
+    print(f"geotags from {filename}:")
+    print(f'[{geotags}]')
+    print(geotags['GPSLatitude'])
+    print(geotags['GPSLongitude'])
     return '{:1.1}{:03d}{:02d}{:04d}{:1.1}{:03d}{:02d}{:04d}'.format(
         geotags['GPSLatitudeRef'],
-        geotags['GPSLatitude'][0][0],
-        geotags['GPSLatitude'][1][0],
-        geotags['GPSLatitude'][2][0],
+        int(geotags['GPSLatitude'][0]),
+        int(geotags['GPSLatitude'][1]),
+        int(100*geotags['GPSLatitude'][2]),
         geotags['GPSLongitudeRef'],
-        geotags['GPSLongitude'][0][0],
-        geotags['GPSLongitude'][1][0],
-        geotags['GPSLongitude'][2][0],
+        int(geotags['GPSLongitude'][0]),
+        int(geotags['GPSLongitude'][1]),
+        int(100*geotags['GPSLongitude'][2]),
     )
 
 def get_date_taken(path):
@@ -105,42 +133,59 @@ def print_output(binary_stream, chunksize):
 def picfilename(radio_id, seq_num):
     return "H{:.5}{:06d}.jpg".format(radio_id, seq_num)
 
-binary_stream = io.BytesIO()
 
-binary_stream.write(bytes(b'\x00\x00\x00\x00')) # Head
-binary_stream.write(bytes(b'\x20\x20\x20\x20\x20')) # Node ID
-binary_stream.write(bytes('ALL       ', 'ASCII')) # Dest
-binary_stream.write(bytes('      ', 'ASCII')) # 6 spaces
-binary_stream.write(bytes(radio_id, 'ASCII')) # Radio ID
-binary_stream.write(bytes('G4TJC     ', 'ASCII')) # Call sign
-binary_stream.write(bytes('      ', 'ASCII')) # 6 spaces
-writedate(binary_stream, datetime.now() - timedelta(hours = 1))
-writedate(binary_stream, datetime.now())
-taken = get_date_taken(exiffilename)
-writedate(binary_stream, taken)
-binary_stream.write(
-    bytes('{:11.11}'.format(os.path.basename(photofilename)), 'ASCII')
-) # Description
-binary_stream.write(bytes('     ', 'ASCII')) # 5 spaces
-binary_stream.write(getfilesize(photofilename))
-outname = picfilename(radio_id,1)
-binary_stream.write(bytes(outname, 'ASCII')) # Call sign
-binary_stream.write(bytes(encodegps(exiffilename), 'ASCII')) # GPS
-binary_stream.write(bytes('        ', 'ASCII')) # 8 spaces
+def write_log(binary_stream, picfile, outdir):
+    binary_stream.write(bytes(b'\x00\x00\x00\x00')) # Head
+    binary_stream.write(bytes(b'\x20\x20\x20\x20\x20')) # Node ID
+    binary_stream.write(bytes('ALL       ', 'ASCII')) # Dest
+    binary_stream.write(bytes('      ', 'ASCII')) # 6 spaces
+    binary_stream.write(bytes(radio_id, 'ASCII')) # Radio ID
+    # binary_stream.write(bytes('G4TJC     ', 'ASCII')) # Call sign
+    # binary_stream.write(bytes('      ', 'ASCII')) # 6 spaces
+    binary_stream.write(bytes(callsign.ljust(16), 'ASCII')) # Callsign in 16-char field
+    writedate(binary_stream, datetime.now() - timedelta(hours = 1))
+    writedate(binary_stream, datetime.now())
+    taken = get_date_taken(picfile)
+    writedate(binary_stream, taken)
+    binary_stream.write(
+        bytes('{:11.11}'.format(os.path.basename(photofilename)), 'ASCII')
+    ) # Description
+    binary_stream.write(bytes('     ', 'ASCII')) # 5 spaces
+    outname = picfilename(radio_id,1)
+    fulloutname =  os.path.join(outdir, 'PHOTO', outname)
+    shrink_image(picfile,fulloutname)
+    binary_stream.write(getfilesize(fulloutname))
+    binary_stream.write(bytes(outname, 'ASCII')) # Call sign
+    binary_stream.write(bytes(encodegps(picfile), 'ASCII')) # GPS
+    binary_stream.write(bytes('        ', 'ASCII')) # 8 spaces
+
+def shrink_image(picfile, saveto):
+    image = Image.open(picfile)
+    image.thumbnail((320,240))
+    image.save(saveto)
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='YSF Image Copy 0.0')
+    print(arguments)
+
+    file_name = arguments['--file']
+    dir_name = arguments['--dir']
+
+    photofilename = file_name
 
 
-print_output(binary_stream, 16)
+    with io.BytesIO() as bs:
+        write_log(bs, file_name, outdir)
 
-#exif = get_exif(photofilename)
-print(outname)
+        print_output(bs, 16)
 
-with open(os.path.join(outdir, 'QSOLOG','QSOPCTDIR.DAT'), 'wb') as f:
-    f.write(binary_stream.getvalue())
-binary_stream.close()
+        #exif = get_exif(photofilename)
+        print(outdir)
 
-with open(os.path.join(outdir, 'QSOLOG','QSOPCTFAT.DAT'), 'wb') as f:
-    f.write(bytes(b'\x40\x00\x00\x00'))
+        with open(os.path.join(outdir, 'QSOLOG','QSOPCTDIR.DAT'), 'wb') as f:
+            f.write(bs.getvalue())
 
+    with open(os.path.join(outdir, 'QSOLOG','QSOPCTFAT.DAT'), 'wb') as f:
+        f.write(bytes(b'\x40\x00\x00\x00'))
 
-copyfile(photofilename, os.path.join(outdir, 'PHOTO', outname))
-
+    # copyfile(photofilename, os.path.join(outdir, 'PHOTO', outname))
