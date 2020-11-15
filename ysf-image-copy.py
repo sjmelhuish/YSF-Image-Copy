c55396cd7c9affb28680ccf62e168fb34e81da47
sjmelhuish <sjmelhuish@users.noreply.github.com>
2020-11-15 13:26:10 +0000

    Try some fonts. Text colour.

--- ysf-image-copy.py
+++ ysf-image-copy.py
@@ -2,7 +2,7 @@
 """ ysf-image-copy
 
 Usage:
-  ysf-image-copy.py CALLSIGN RADIOID OUTDIR [-d DIRECTORY|-f PICFILE] [-u]
+  ysf-image-copy.py CALLSIGN RADIOID OUTDIR [-d DIRECTORY|-f PICFILE] [-u] [-t TEXT] [-c COLOUR]
   
 Arguments:
   CALLSIGN            The RX location from internal list of RAS sites
@@ -13,7 +13,9 @@ Options:
   -h --help                          Show this screen
   -v --version                       Show version
   -d DIRECTORY --dir=DIRECTORY       Name the input directory forbatch conversion
-  -f PICFILE --file=PICFILE         Convert a single file
+  -f PICFILE --file=PICFILE          Convert a single file
+  -t TEXT --text=TEXT                Write text over image
+  -c COLOUR --colour=COLOUR          Colour for the text
   -u                                 Update files at outdir instead of starting from scratch
   
 """
@@ -24,14 +26,15 @@ from datetime import datetime, timedelta
 import os
 from PIL import Image, ExifTags
 from PIL.ExifTags import TAGS, GPSTAGS
-import pyguetzli
-
-from shutil import copyfile # Used to copy jpg. Not if we write it?
+from PIL import ImageFont
+from PIL import ImageDraw 
+from colour import Color
+#from shutil import copyfile # Used to copy jpg. Not if we write it?
 
 from docopt import docopt
 
-photofilename = r'TestImages/StileNoExif.JPG'
-exiffilename = r'TestImages/StileExif.JPG'
+# photofilename = r'TestImages/StileNoExif.JPG'
+# exiffilename = r'TestImages/StileExif.JPG'
 outdir = r'TestOut'
 
 radio_id = 'E0Spx'
@@ -80,25 +83,37 @@ def encodegps(filename):
     exif = get_exif(filename)
     # print(f"exif from {filename}:")
     # print(exif)
-    geotags = get_geotagging(exif)
-    print(f"geotags from {filename}:")
-    print(f'[{geotags}]')
-    print(geotags['GPSLatitude'])
-    print(geotags['GPSLongitude'])
-    return '{:1.1}{:03d}{:02d}{:04d}{:1.1}{:03d}{:02d}{:04d}'.format(
-        geotags['GPSLatitudeRef'],
-        int(geotags['GPSLatitude'][0]),
-        int(geotags['GPSLatitude'][1]),
-        int(100*geotags['GPSLatitude'][2]),
-        geotags['GPSLongitudeRef'],
-        int(geotags['GPSLongitude'][0]),
-        int(geotags['GPSLongitude'][1]),
-        int(100*geotags['GPSLongitude'][2]),
-    )
+    try:
+        geotags = get_geotagging(exif)
+    except ValueError:
+        # No EXIF GPS data
+        return "                    "
+    try:
+        print(f"geotags from {filename}:")
+        print(f'[{geotags}]')
+        print(geotags['GPSLatitude'])
+        print(geotags['GPSLongitude'])
+        return '{:1.1}{:03d}{:02d}{:04d}{:1.1}{:03d}{:02d}{:04d}'.format(
+            geotags['GPSLatitudeRef'],
+            int(geotags['GPSLatitude'][0]),
+            int(geotags['GPSLatitude'][1]),
+            int(100*geotags['GPSLatitude'][2]),
+            geotags['GPSLongitudeRef'],
+            int(geotags['GPSLongitude'][0]),
+            int(geotags['GPSLongitude'][1]),
+            int(100*geotags['GPSLongitude'][2]),
+        )
+    except KeyError:
+        # GPS data missing?
+        return "                    "
 
 def get_date_taken(path):
-    dto_str = Image.open(path)._getexif()[36867]
-    return datetime.strptime(dto_str, '%Y:%m:%d %H:%M:%S')
+    try:
+        dto_str = Image.open(path)._getexif()[36867]
+        return datetime.strptime(dto_str, '%Y:%m:%d %H:%M:%S')
+    except KeyError:
+        # No EXIF data for datetime
+        return datetime.now()
 
 def get_exif(filename):
     image = Image.open(filename)
@@ -118,7 +133,7 @@ def writedate(binary_stream, when):
     t = when.timetuple()
     for z in t[:6]:
         n = dec2hex(z)
-        print("{:02d} -> {:02d} (0x{:02x})".format(z,n,n))
+        # print("{:02d} -> {:02d} (0x{:02x})".format(z,n,n))
         binary_stream.write (n.to_bytes(1, byteorder='big', signed=False))
 
 def print_output(binary_stream, chunksize):
@@ -134,7 +149,8 @@ def picfilename(radio_id, seq_num):
     return "H{:.5}{:06d}.jpg".format(radio_id, seq_num)
 
 
-def write_log(binary_stream, picfile, outdir):
+def write_log(binary_stream, picfile, outdir, picnum, text, colour):
+    print(f'Write log entry for {picfile}')
     binary_stream.write(bytes(b'\x00\x00\x00\x00')) # Head
     binary_stream.write(bytes(b'\x20\x20\x20\x20\x20')) # Node ID
     binary_stream.write(bytes('ALL       ', 'ASCII')) # Dest
@@ -148,21 +164,61 @@ def write_log(binary_stream, picfile, outdir):
     taken = get_date_taken(picfile)
     writedate(binary_stream, taken)
     binary_stream.write(
-        bytes('{:11.11}'.format(os.path.basename(photofilename)), 'ASCII')
+        bytes('{:11.11}'.format(os.path.basename(picfile)), 'ASCII')
     ) # Description
     binary_stream.write(bytes('     ', 'ASCII')) # 5 spaces
-    outname = picfilename(radio_id,1)
+    outname = picfilename(radio_id, picnum)
     fulloutname =  os.path.join(outdir, 'PHOTO', outname)
-    shrink_image(picfile,fulloutname)
+    print(f'Convert {picfile} -> {outname}')
+    shrink_image(picfile,fulloutname, text, colour)
     binary_stream.write(getfilesize(fulloutname))
     binary_stream.write(bytes(outname, 'ASCII')) # Call sign
     binary_stream.write(bytes(encodegps(picfile), 'ASCII')) # GPS
     binary_stream.write(bytes('        ', 'ASCII')) # 8 spaces
 
-def shrink_image(picfile, saveto):
-    image = Image.open(picfile)
+def load_font():
+    fonts = [
+        'verdana.ttf',
+        'helvetica.ttf',
+        'arial.ttf',
+        'tahoma.ttf',
+        'sans-serif.ttf',
+    ]
+
+    for font_name in fonts:
+        try:
+            font = ImageFont.truetype(font_name, 48)
+            return font
+        except OSError as e:
+            print("Font problem.", e, font_name)
+
+    return ImageFont.load_default()
+
+
+def shrink_image(picpath, saveto, text, colour):
+    print(f'{picpath} -> {saveto}')
+    image = Image.open(picpath)
     image.thumbnail((320,240))
+    draw = ImageDraw.Draw(image)
+    font = load_font()
+    with_newlines = text.replace('\\','\n')
+    c = Color(colour)
+    ct = tuple(int(255*v) for v in c.rgb)
+    draw.text((0,0), with_newlines,ct,font=font)
     image.save(saveto)
+
+def process_pic(file_name, outdir, picnum):
+    with io.BytesIO() as bs:
+        write_log(bs, file_name, outdir, picnum)
+
+        #print_output(bs, 16)
+
+        #exif = get_exif(photofilename)
+        #print(outdir)
+
+        with open(os.path.join(outdir, 'QSOLOG','QSOPCTDIR.DAT'), 'wb') as f:
+            f.write(bs.getvalue())
+
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='YSF Image Copy 0.0')
@@ -170,22 +226,35 @@ if __name__ == '__main__':
 
     file_name = arguments['--file']
     dir_name = arguments['--dir']
+    text = arguments['--text']
+    colour = arguments['--colour']
 
-    photofilename = file_name
+    if colour == None:
+        colour = 'red' 
 
+    next_pic_num = 1
 
     with io.BytesIO() as bs:
-        write_log(bs, file_name, outdir)
+        if file_name is not None:
+            write_log(bs, file_name, outdir, next_pic_num, text, colour)
+            next_pic_num += 1
 
-        print_output(bs, 16)
-
-        #exif = get_exif(photofilename)
-        print(outdir)
+        if dir_name is not None:
+            for filename in os.listdir(dir_name):
+                try:
+                    fullfname = os.path.join(dir_name, filename)
+                    write_log(bs, fullfname, outdir, next_pic_num, text, colour)
+                    next_pic_num += 1
+                except IOError as e:
+                    print("cannot convert", filename, e)
 
         with open(os.path.join(outdir, 'QSOLOG','QSOPCTDIR.DAT'), 'wb') as f:
             f.write(bs.getvalue())
-
+            
     with open(os.path.join(outdir, 'QSOLOG','QSOPCTFAT.DAT'), 'wb') as f:
-        f.write(bytes(b'\x40\x00\x00\x00'))
+        for pnum in range(next_pic_num):
+            f.write(bytes(b'\x40'))
+            addr = 0X80 * pnum
+            f.write(addr.to_bytes(3, byteorder='big', signed=False))
 
     # copyfile(photofilename, os.path.join(outdir, 'PHOTO', outname))
